925836dea8f5bdca1b1d46575e82108029057f49
liy <cainyue@gmail.com>
2026-02-26 09:17:06 +0800

    test: finalize test setup

--- src/test/basic.test.js
+++
@@ -1,8 +0,0 @@
-// Basic test to verify setup
-import { describe, it, expect } from 'vitest';
-
-describe('Basic Setup', () => {
-  it('should pass', () => {
-    expect(true).toBe(true);
-  });
-});
